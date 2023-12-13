c1811e9483da7b0896ff4cdbb7f0cd7e75a56c35
Brayan17king <104658332+Brayan17king@users.noreply.github.com>
2023-12-13 12:55:08 -0500

    feat: :sparkles: Js file for logic

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function imgSlider(anithing){
+    document.querySelector(".starbucks").src = anithing;
+}
+
+function changecircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.backgroundColor = color;
+}
+
+function changeColor(color){
+    const parrafo = document.querySelector('.span');
+    parrafo.style.color = color;
+}
+
+function changeText(color){
+    const texto = document.querySelector('#letra');
+    texto.style.backgroundColor = color;
+}
